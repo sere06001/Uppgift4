c5fe3ab09077419a1d6462dd0bc437f6c72c1cdd
sere06001 <sere06001@utb.vaxjo.se>
2024-02-02 12:59:12 +0100

    Add project files.

---
+++ Uppgift4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+namespace uppgift4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv 3 ord");
+            string[] ord = new string[3];
+            for (int i = 0; i < ord.Length; i++)
+            {
+                ord[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+            Console.Write("Vilket tecken vill du söka efter i alla ord?: ");
+            char tecken = Convert.ToChar(Console.ReadLine());
+            if (FinnsTecknetIAllaSträngar(ord, tecken) == true)
+            {
+                Console.WriteLine("Alla ord innehåller detta tecken");
+            }
+            else
+            {
+                Console.WriteLine("Inte alla ord innehåller detta tecken");
+            }
+            Console.ReadKey();
+        }
+        static bool FinnsTecknetIAllaSträngar(string[] a, char b)
+        {
+            bool c = false;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i].Contains(b))
+                {
+                    c = true;
+                }
+                else
+                {
+                    c = false;
+                    break;
+                }
+            }
+            return c;
+        }
+    }
+}
